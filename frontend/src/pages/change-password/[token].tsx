7c952d3494b8c53914f2d9cb3d5e462f6eb2fc7d
Adam Spice <adam@adam-spice.online>
2021-02-07 04:20:21 +1300

    added type orm

--- frontend/src/pages/change-password/[token].tsx
+++ frontend/src/pages/change-password/[token].tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link, Text } from "@chakra-ui/react";
+import { Button, Flex, Link, Text } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { NextPage } from "next";
 import { withUrqlClient } from "next-urql";
